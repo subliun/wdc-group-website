d849bd828c8f81e8971fb476c65235381eed0e6b
Obadiah Crowe <me@obadiah.io>
2019-06-11 01:52:31 +0100

    Fixing up sql

--- server/populate.sql
+++ server/populate.sql
@@ -1,54 +1,67 @@
-DROP TABLE IF EXISTS restauarant_images;
-DROP TABLE IF EXISTS reviews;
-DROP TABLE IF EXISTS bookings; 
-DROP TABLE IF EXISTS restaurants; 
-DROP TABLE IF EXISTS users;
-
 USE restaurant_db;
-CREATE TABLE restaurants (
-    restaurant_id int NOT NULL AUTO_INCREMENT,
-    description text,
-    location text,
-    rating int, /* out of 100 */
-    latitiute int,
-    longitude int,
-    num_tables int,
-    PRIMARY KEY (restaurant_id)
+
+CREATE TABLE IF NOT EXISTS restaurants (
+    id          LONG            NOT NULL        AUTO_INCREMENT,
+
+    `name`      VARCHAR(64)     NOT NULL,
+    `desc`      VARCHAR(256),
+    location    VARCHAR(256),
+    rating      INT,
+    latitude    INT,
+    longitude   INT,
+    tabSize     INT,
+
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE restauarant_images (
-    image_id int NOT NULL AUTO_INCREMENT,
-    restaurant_id int NOT NULL,
-    path text,
-    PRIMARY KEY (image_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+CREATE TABLE IF NOT EXISTS restaurant_images (
+    id          LONG           NOT NULL         AUTO_INCREMENT,
+
+    rest_id     LONG           NOT NULL,
+    path        TEXT,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (rest_id) REFERENCES restaurants(id) ON DELETE CASCADE
 );
 
-CREATE TABLE reviews (
-    review_id int NOT NULL AUTO_INCREMENT,
-    restaurant_id int NOT NULL,
-    reviewer_name text,
-    rating int, /* out of 100 */
-    review_text text,
-    PRIMARY KEY (review_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+CREATE TABLE IF NOT EXISTS restaurant_reviews (
+    id          LONG            NOT NULL       AUTO_INCREMENT,
+
+    rest_id     LONG            NOT NULL,
+    user_id     LONG            NOT NULL,
+
+    rating      INT             NOT NULL,
+    review      TEXT,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (rest_id) REFERENCES restaurants(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE users (
-    user_id int NOT NULL AUTO_INCREMENT,
-    email text,
-    password text, 
-    PRIMARY KEY (user_id)
+CREATE TABLE IF NOT EXISTS users (
+    id          LONG            NOT NULL        AUTO_INCREMENT,
+
+    email       VARCHAR(256)    NOT NULL,
+    password    VARCHAR(512)    NOT NULL,
+
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE bookings (
-    booking_id int NOT NULL AUTO_INCREMENT, 
-    restaurant_id int NOT NULL,
-    user_id int NOT NULL,
-    table_number int, 
-    date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    cancelled boolean NOT NULL DEFAULT 0,
-    PRIMARY KEY (booking_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE IF NOT EXISTS restaurant_bookings (
+    id          LONG            NOT NULL        AUTO_INCREMENT,
+
+    rest_id     LONG            NOT NULL,
+    user_id     LONG            NOT NULL,
+    tabNum      INT             NOT NULL,
+
+    `time`      TIMESTAMP       NOT NULL        DEFAULT NOW(),
+    cancelled   BIT             NOT NULL        DEFAULT 0,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (rest_id) REFERENCES restaurants(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+
+
+
